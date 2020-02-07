93a87d10ce34cc7b8b2ddcc8aff908134e38e428
Takuya Natsume <xxtyzs@gmail.com>
2020-02-07 12:37:21 +0900

    Added layout file and index.blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends("layout/app")
+
+@section("content")
+
+    <h1>layout is working</h1>
+
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Todo App</title>
+</head>
+
+<body>
+<nav class="navbar navbar-light bg-dark">
+    <a class="navbar-brand mb-0 text-white h1" href="/">Todo App</a>
+</nav>
+
+@yield("content")
+
+</body>
+</html>
