4994e795c461beeae71ad56ad49161cb032fa456
Takuya Natsume <xxtyzs@gmail.com>
2020-03-02 21:52:46 +0900

    Still refactoring...

--- app/Repository/TodoRepository.php
+++ app/Repository/TodoRepository.php
@@ -4,7 +4,7 @@
 namespace App\Repository;
 
 
-use App\Todo;
+use App\Model\Todo;
 
 class TodoRepository
 {
--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\User;
+use App\Model\User;
 
 class UserRepository
 {
